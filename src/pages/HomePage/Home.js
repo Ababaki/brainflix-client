b4b73eee8c4420d52461a60883251366fe2d4145
Ababaki <124071121+Ababaki@users.noreply.github.com>
2023-03-19 18:00:35 -0400

    Remove Comments

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -8,30 +8,26 @@ import axios from 'axios';
 const API_URL = "http://localhost:7070";
 
 const Home = () => {
-  // [state, setState] = usState(initial value) reminder charity case 
   const [videos, setVideos] = useState([]); 
   const [activeVideo, setActiveVideo] = useState(null);
 
-  const { id } = useParams(); // id is set when there is an id param in the url
+  const { id } = useParams();
 
-  // runs after first render
+  
   useEffect(() => {
-    axios.get(API_URL + '/videos') // get all videos
+    axios.get(API_URL + '/videos') 
       .then(res => setVideos(res.data)) 
       .catch(error => console.log(error));
   }, []);
 
-  // runs after get videos/ and after first render
+
   useEffect(() => { 
-    // prevent using video[0] (default) only use it if no id in the url
     if(videos.length > 0){
-      // condition ? result if condition is true : result if condition is false (ternary operator)
-      axios.get(API_URL + '/videos/' + (id ? id : videos[0].id)) // get video by id
+      axios.get(API_URL + '/videos/' + (id ? id : videos[0].id)) 
         .then(res => setActiveVideo(res.data))
         .catch(error => console.log(error));
     }
-  }, [videos, id]); // videos goes from [] -> [vid1, vid2 ... ] when this happens need to fetch inital active video
-
+  }, [videos, id]); 
   if(activeVideo === null || videos === []) {
     return <h1>Loading...</h1>
   }
